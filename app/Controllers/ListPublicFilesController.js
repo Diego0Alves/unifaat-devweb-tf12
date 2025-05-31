670669907cfd07f9a5388de4d00dc0bf15cb43ea
Diego Alves <anibodyelse1@gmail.com>
2025-05-31 13:25:57 -0300

    Update ./app/Controllers/ListPublicFilesController.js

--- app/Controllers/ListPublicFilesController.js
+++ app/Controllers/ListPublicFilesController.js
@@ -1,6 +1,9 @@
-import { join } from 'path';
+import { join, parse } from 'path';
 import { readdir } from 'fs';
-import TodoModel from '../Models/TodoModel.js';
+import PessoaModel from '../Models/PessoaModel.js';
+import TelefoneModel from '../Models/TelefoneModel.js';
+import CONSTANTS from '../../config/constants.js';
+
 
 export default async (request, response) => {
 
@@ -8,7 +11,24 @@ export default async (request, response) => {
 
     const ORDER_DIRECAO = ['desc', 'asc'];
 
-    const dirPath = join(CONSTANTS.DIR, 'public');
+    const HTTP_STATUS = CONSTANTS.HTTP;
+
+    const limit = parseInt(request.query.limit) || 10;
+    const offset = parseInt(request.query.offset) || 0;
+    const orderBy = request.query.orderBy || 'id,asc';
+
+    if (limit > CONSTANTS.MAX_GET_LIMIT) {
+        return response.status(CONSTANTS.HTTP.BAD_REQUEST).json({ error: `Limite máximo de itens: ${CONSTANTS.MAX_GET_LIMIT} excedido`});
+    }
+
+    const orderByArray = orderBy.split(',');
+
+    const orderCampo = orderByArray[0];
+    const orderDirecao = orderByArray[1] || 'asc';
+
+    const orderCampoFim = ORDER_CAMPOS.includes(orderCampo) ? orderCampo : 'id';
+
+    const orderDirecaoFim = ORDER_DIRECAO.includes(orderDirecao) ? orderDirecao : 'asc';
 
     readdir(dirPath, (err, files) => {
         if (err) {
